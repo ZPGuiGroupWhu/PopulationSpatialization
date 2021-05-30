da9302159b3f8a29e658ee8049421d8e6f1a337c
meiyuao <meiyuao@qq.com>
2021-05-30 20:49:13 +0800

    add description and data

--- Code/populationSpatialization.py
+++ Code/populationSpatialization.py
@@ -1,24 +1,37 @@
 import pandas as pd
 import numpy as np
-from config import *
+import json
+from conv import *
+from utils import *
+from sklearn.ensemble import RandomForestRegressor
 
-relative_src = 'result_relative.txt'
-absolute_src = 'result_absolute.txt'
 
-tmp = pd.read_table("community_county.txt",sep=',',index_col='gid')
-tmp = tmp.sort_index()
-for row in tmp.iterrows():
-    if row[1][0] in zhuchengqu:
-        zhuchengqu_community.append(row[0])
-    if row[1][0] in yuanchengqu:
-        yuanchengqu_community.append(row[0])
+# 100m
+RESOLUTION = 100
+N_ROW = 1540
+N_COLUMN = 1250
+
+## 200m
+# RESOLUTION = 200
+# N_ROW = 770
+# N_COLUMN = 625
+
+## 500m
+# RESOLUTION = 500
+# N_ROW = 308
+# N_COLUMN = 250
+
+relative_src = 'resource/feature_vector_of_pixel_grade_proportions_' + str(RESOLUTION) +'.txt'
+county_src = r"resource/wuhanCountyPop.txt"
+sub_src =  r"resource/wuhanSubDistrictPop.txt"
+sub_pop =  r"resource/sub_pop.txt"
+sub_pd =  r"resource/sub_population_density.txt"
+
 subPop = pd.read_table(sub_pop,sep=',',index_col='gid')
 subPop = subPop.sort_index()
 
-sub_midu = pd.read_table(sub_midu,sep=',',index_col='gid')
-sub_midu = sub_midu.sort_index()
-community_midu = pd.read_table(community_midu,sep=',',index_col='gid')
-community_midu = community_midu.sort_index()
+sub_pd = pd.read_table(sub_pd,sep=',',index_col='gid')
+sub_pd = sub_pd.sort_index()
 
 
 subPara_relative = pd.read_table(relative_src,sep=',',index_col='subid')
@@ -26,15 +39,8 @@ subPara_relative = subPara_relative.sort_index()
 
 
 
-import matplotlib.pyplot as plt
 
-
-
-from conv import *
-from utils import *
-import random
-
-
+grid_src = r"..\Data\grid" + str(RESOLUTION) +".csv"
 gridInfo = pd.read_table(grid_src,sep=',',index_col='id')
 gridInfo = gridInfo.sort_index()
 
@@ -49,18 +55,10 @@ sub_dict = subInfo.to_dict()['subPopNum']
 index_matrix = np.array(gridInfo.index).reshape((N_ROW,N_COLUMN))[::-1]#索引的二维矩阵
 county_matrix = np.array(gridInfo['county_id']).reshape((N_ROW,N_COLUMN))[::-1]#countyid的二维矩阵
 sub_matrix = np.array(gridInfo['sub_id']).reshape((N_ROW,N_COLUMN))[::-1]#subid的二维矩阵
-has_juminhouse_index = np.array(gridInfo['has_juminhouse']).reshape((N_ROW,N_COLUMN))[::-1]#是否又建筑物的二维矩阵
 building_area_matrix = np.array(gridInfo['building_area']).reshape((N_ROW,N_COLUMN))[::-1]#是否又建筑物的二维矩阵
 
-subOfCounty = {}
-for index,row in subInfo.iterrows():
-    subOfCounty[row['countyId']] = {}
-for index,row in subInfo.iterrows():
-    subOfCounty[row['countyId']][index] = row['subPopNum']
 
-
-import json
-f = open("class.txt",'r')
+f = open("resource/class_"+ str(RESOLUTION) +".txt",'r')
 classes = json.loads(f.read())
 f.close()
 result_matrixs =[]
@@ -87,7 +85,6 @@ for mat in result_matrixs:
 
 
 
-import jenkspy
 poi1_matrix = np.array(gridInfo['count_poi1']).reshape((N_ROW,N_COLUMN))[::-1]
 poi2_matrix = np.array(gridInfo['count_poi2']).reshape((N_ROW,N_COLUMN))[::-1]
 
@@ -106,8 +103,8 @@ night_point_matrix = np.array(gridInfo['mobile_night']).reshape((N_ROW,N_COLUMN)
 night_point_matrix[np.isnan(night_point_matrix)] = 0
 
 
-noPoiNoBuilding = ((np.isnan(sub_matrix)==False)&(has_juminhouse_index == 0) & (poi_matrix ==0))
-noPoiHasBuilding = ((np.isnan(sub_matrix)==False)&(has_juminhouse_index == 1) & (poi_matrix ==0) &(night_point_matrix==0))
+noPoiNoBuilding = ((np.isnan(sub_matrix)==False)&(building_area_matrix == 0) & (poi_matrix ==0))
+noPoiHasBuilding = ((np.isnan(sub_matrix)==False)&(building_area_matrix > 0) & (poi_matrix ==0))
 
 
 
@@ -121,36 +118,25 @@ tmp1_matrix += 1
 
 
 
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import ExtraTreesRegressor
 
-fc = 0
-sum = 0
-max  = 100000
-sum_r2 = 0
-sum_MAE=0
-sum_RMSE = 0
-max_MAE =1000000
-max_RMSE = 1000000000
 county = {11: 0, 18: 0, 15: 0, 14: 0, 12: 0, 6: 0, 10: 0, 3: 0, 1: 0, 16: 0, 4: 0, 5: 0, 7: 0, 9: 0, 8: 0, 17: 0, 2: 0}
-var=0
 for i in range(1):
     rfc = RandomForestRegressor(max_features='sqrt',n_estimators=250,min_samples_leaf=1,
-                                oob_score=True,criterion="mse",bootstrap=True,random_state=2)
-    #################################百分比 密度
-    rfc.fit(subPara_relative, sub_midu)
+                                oob_score=True,criterion="mse",bootstrap=True,random_state=i)
 
+    rfc.fit(subPara_relative, sub_pd)
     result = rfc.predict(np.hstack(result_matrixs)).reshape((N_ROW,N_COLUMN))
 
     # building patch data
+    _ll = list(result[poi_matrix == 0])
+    _ll.sort()
     tmp1_matrix[noPoiHasBuilding] = wei_matrix[noPoiHasBuilding] / max1
-    #
     tmp1_matrix[noPoiHasBuilding] *= _ll[0]*3
-
     result[noPoiHasBuilding] = tmp1_matrix[noPoiHasBuilding]
+    result[noPoiNoBuilding] = 0
     # #
 
-    # # mobile positioning data
+    # mobile positioning data
     x = list(result[poi_matrix > 0])
     x.sort()
     y = list(night_point_matrix[poi_matrix > 0])
@@ -173,14 +159,12 @@ for i in range(1):
                 result[i,j] = x[int(len(x)*(index_y*0.5+index_x*0.5))]
 
 
+    # spatial filtering
     result = conv(result, gau(3), county_matrix, 0)
 
+
     weight_matrix = normalize(result,county_matrix,county_dict)
-    # # print(np.min(weight_matrix))
-
-
     pop_matrix = calCountyPop(weight_matrix,county_matrix,county_dict)
-
     pop_dict = {}
     [rows, cols] = pop_matrix.shape
     for i in range(rows):
@@ -194,29 +178,8 @@ for i in range(1):
                 else:
                     pop_dict[county_matrix[i, j]] = []
 
-
-
-
-    var += math.sqrt(np.var(pop_matrix[np.isnan(county_matrix) == False] / 0.01))
     MAE, RMSE ,r2= calSubError(pop_matrix,sub_matrix,sub_dict)
-    print(MAE)
-
-    # outGridPop(pop_matrix,index_matrix,county_matrix)
-    sum_MAE += MAE
-    if max_MAE > MAE:
-        max_MAE = MAE
-    sum_RMSE += RMSE
-    if max_RMSE > RMSE:
-        max_RMSE = RMSE
-
-    sum_r2 += r2
-print("MAE mean：" + str(sum_MAE / 10))
-
-print("MAE min：" + str(max_MAE))
-print("RMSE mean：" + str(sum_RMSE / 10))
-print("var mean：" + str(var / 10))
-
-print("RMSE min：" + str(max_RMSE))
-print("r2 mean：" + str(sum_r2/10))
-print("fc mean：" + str(fc/10))
+    print("MAE :" + str(MAE))
+    print("RMSE :" + str(RMSE))
+    print("r2 :" + str(r2))
 
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PopulationSpatialization
+
+
+## Introduction
+
+## Data
+
+
+Data contains the pixel-level attribute statistics required for regression modeling. Experimental area (Wuhan) is divided into pixels with 500m (308 x 250), 200m (770 x 625) and 100m (1540 x 1250) resolution  respectively. In each gridxxx.csv, the fields used are explained in the following table.
+
+
+|  Field   | Description  |  Field   | Description  |
+|  ----  | ----  |  ----  | ----  |
+| count_poi1  | Leisure and entertainment | count_poi2  | Accommodation |
+| count_poi3  | Parking lot | count_poi7  | Medical service |
+| count_poi8  | Hospital | count_poi11  | Residential community |
+| count_poi14  | Government agency | count_poi18  | Auto service |
+| count_poi21  | Research and education | count_poi22  | Shopping |
+| count_poi23  | Financial services | count_poi24  | Restaurant |
+| building_area  | area of building patch data | mobile_night  | counts of mobile positioning data |
+| sub_id  | id of the street that pixel belongs to | county_id  | id of the district that pixel belongs to |
+
+
+## Code
+
+#### Implementation Environment
+- Python 3.x
+- You need to import numpy, pandas and sklearn
+
+#### How To Run The Code
+
+In populationSpatialization.py, you need to set RESOLUTION, N_ROW and N_COLUMN first for choosing pixel resolution, and then you can run populationSpatialization.py to implent population estimation.
+
+## Result
+Result contains the predicted population of three resolutions (500m, 200m and 100m).The id and the predicted population of the pixel are listed in the file.
